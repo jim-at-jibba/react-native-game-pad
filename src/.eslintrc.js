a93ed117ca42c487980a0dc49ffc2c965fa34a08
James Best <jim@justjibba.net>
2021-02-23 06:02:25 +0000

    Add eslint and prettier

---
+++ src/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    root: true,
+    extends: [
+        "@react-native-community",
+        "airbnb-typescript",
+        "prettier",
+        "prettier/@typescript-eslint",
+        "prettier/react"
+    ],
+    rules: {}
+}
